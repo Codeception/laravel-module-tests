f50a831e834cc922ed5d7cfa4192d3acdbbebdb9
Tavo Nieves J <ganieves@outlook.com>
2021-02-09 11:02:54 -0500

    Update default mailer

--- config/mail.php
+++ config/mail.php
@@ -6,20 +6,14 @@ return [
 
     // Default Mailer
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'log'),
 
     // Mailer Configurations
 
     'mailers' => [
-        'smtp' => [
-            'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'auth_mode' => null,
+        'log' => [
+            'transport' => 'log',
+            'channel' => env('MAIL_LOG_CHANNEL'),
         ],
     ],
 

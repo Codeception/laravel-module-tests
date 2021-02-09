dde97429cac08cfa69d4d41e5037c73a91c612fd
Tavo Nieves J <ganieves@outlook.com>
2021-02-09 11:43:29 -0500

    Update default mailer

--- config/mail.php
+++ config/mail.php
@@ -6,15 +6,7 @@ return [
 
     // Mail Driver
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
-
-    // SMTP Host Address
-
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-
-    // SMTP Host Port
-
-    'port' => env('MAIL_PORT', 587),
+    'driver' => env('MAIL_DRIVER', 'log'),
 
     // Global "From" Address
 
@@ -22,20 +14,6 @@ return [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
-
-    // E-Mail Encryption Protocol
-
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-
-    // SMTP Server Username
-
-    'username' => env('MAIL_USERNAME'),
-
-    'password' => env('MAIL_PASSWORD'),
-
-    // Sendmail System Path
-
-    'sendmail' => '/usr/sbin/sendmail -bs',
 
     // Markdown Mail Settings
 

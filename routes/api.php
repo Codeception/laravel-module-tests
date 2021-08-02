854016478dbcbaa7c7594b4ccbfef61e59d5043c
Fernando Küpper Cardoso <fernando@kupper.com.br>
2021-08-02 17:46:11 +0200

    test: uploaded files (#5)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/upload-files', [TestController::class, 'uploadFiles']);

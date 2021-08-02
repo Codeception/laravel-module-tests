e1ad3977e26d12e8c6144e67c8809026b0454d58
Fernando Kupper Cardoso <fernando@kupper.com.br>
2021-08-02 17:43:32 +0200

    chore: use JSON_THROW_ON_ERROR flag

--- tests/_build/support/Helper/CustomRequests.php
+++ tests/_build/support/Helper/CustomRequests.php
@@ -6,6 +6,8 @@ namespace Tests\Helper;
 
 use Codeception\Module;
 use Codeception\Module\Laravel;
+use JsonException;
+use Throwable;
 
 final class CustomRequests extends Module
 {
@@ -14,11 +16,10 @@ final class CustomRequests extends Module
         /** @var Laravel $laravelModule */
         $laravelModule = $this->getModule('Laravel');
         $response = $laravelModule->_request('POST', '/api/upload-files', [], $files);
-        $response = json_decode($response, true);
-
-        $last_error = json_last_error();
-        if ($last_error !== JSON_ERROR_NONE) {
-            $this->fail("Failed to parse response from uploaded-files endpoint with json error code {$last_error}");
+        try {
+            $response = json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            $this->fail("Failed to decode json response from uploaded-files endpoint with json error code {$e->getCode()}");
         }
 
         return $response;

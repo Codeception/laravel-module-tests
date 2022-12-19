94aa22117757bfa1d62a8d1e7d7c1f5f371f69b7
LucianDex <remindfwd@gmail.com>
2022-12-19 15:05:13 +0200

    Added id on logout link
    - it will be used in tests

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -49,7 +49,7 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            <a id="logout" class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}

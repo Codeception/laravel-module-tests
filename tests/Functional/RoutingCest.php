1fb00c6b6387fcbb83c878af5089ad2c10cf3bab
luciandex <remindfwd@gmail.com>
2023-01-03 15:37:26 +0200

    Updated logout method. Reverted provious changes to EventCest. Make use Config in RoutingCest

--- tests/Functional/RoutingCest.php
+++ tests/Functional/RoutingCest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Functional;
 
+use Illuminate\Contracts\Config\Repository as Config;
 use App\Http\Controllers\TestController;
 use Tests\FunctionalTester;
 
@@ -25,13 +26,13 @@ final class RoutingCest
     {
         $I->amOnPage('/test-value');
         $I->seeCurrentActionIs(TestController::class. '@testValue');
-        $I->see('Test value is: ' . config('test_value'));
+        $I->see('Test value is: ' . app()->get(Config::class)->get('test_value'));
     }
 
     public function seeCurrentRouteIs(FunctionalTester $I)
     {
         $I->amOnAction(TestController::class. '@testValue');
         $I->seeCurrentRouteIs('test-value');
-        $I->see('Test value is: ' . config('test_value'));
+        $I->see('Test value is: ' . app()->get(Config::class)->get('test_value'));
     }
 }
